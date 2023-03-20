0a127b4a708d4ec45024f0142413b348d303572d
gyt <plzfgme@ustc.edu.cn>
2023-03-20 15:25:41 +0800

    bench(ggm): use random key

--- benches/ggm.rs
+++ benches/ggm.rs
@@ -1,15 +1,39 @@
-use cprf::ggm::GgmRCPrfMasterKey;
-use criterion::{criterion_group, criterion_main, Criterion, BenchmarkId, black_box};
+use std::ops::Range;
+
+use cprf::ggm::{GgmRCPrfConstrainedKey, GgmRCPrfMasterKey};
+use criterion::{black_box, criterion_group, criterion_main, BatchSize, BenchmarkId, Criterion};
 use generic_array::GenericArray;
+use rand::{rngs::OsRng, RngCore};
+
+fn get_ck(range: Range<u64>) -> GgmRCPrfConstrainedKey {
+    let mut key = GenericArray::from([0u8; 16]);
+    OsRng.fill_bytes(&mut key);
+    let mk = GgmRCPrfMasterKey::new(key);
+
+    mk.constrained(range)
+}
 
 fn bench_ggm_evaluate_all(c: &mut Criterion) {
-    let mut group = c.benchmark_group("GGM evaluate_all");
+    let mut group = c.benchmark_group("GGM ck evaluate vs evaluate_all");
     for i in [10u64, 100u64, 1000u64, 5000u64].iter() {
-        let key = GenericArray::from([0u8; 16]);
-        let mk = GgmRCPrfMasterKey::new(key);
-        let ck = mk.constrained(2000..2000+i+1);
-        group.bench_function(BenchmarkId::new("evalate for each", i), |b| b.iter(|| (ck.get_range().0..=ck.get_range().1).for_each(|x| {ck.evaluate(black_box(x));})));
-        group.bench_function(BenchmarkId::new("evalate_all", i), |b| b.iter(|| ck.evaluate_all().for_each(|_| ())));
+        group.bench_function(BenchmarkId::new("evalate for each", i), |b| {
+            b.iter_batched(
+                || get_ck(2000..2000 + i + 1),
+                |ck| {
+                    (ck.get_range().0..=ck.get_range().1).for_each(|x| {
+                        ck.evaluate(black_box(x));
+                    })
+                },
+                BatchSize::SmallInput,
+            );
+        });
+        group.bench_function(BenchmarkId::new("evalate_all", i), |b| {
+            b.iter_batched(
+                || get_ck(2000..2000 + i),
+                |ck| ck.evaluate_all().for_each(|_| ()),
+                BatchSize::SmallInput,
+            )
+        });
     }
 }
 
